68969056fc7cb014ab2a36de924daa1dadae80bd
Bipin <bipinchoubey771@gmail.com>
2026-02-15 03:31:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,52 +1,53 @@
 class Solution {
     class Pair{
-        int src;
-        int dest;
+        int node;
+        int parent;
         
-        Pair(int src,int dest){
-            this.src = src;
-            this.dest = dest;
+        Pair(int node, int parent){
+            this.node = node;
+            this.parent = parent;
         }
     }
+    
     public boolean isCycle(int V, int[][] edges) {
         // Code here
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         
         for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
         
-        for(int[] row : edges){
-            int u = row[0];
-            int v = row[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+        for(int[] edge : edges){
+            int row = edge[0];
+            int col = edge[1];
+            
+            adj.get(row).add(col);
+            adj.get(col).add(row);
         }
         
         int[] visited = new int[V];
         
         for(int i = 0; i < V; i++){
-            if(visited[i] == 0){
-                if(CheckCycle(i,visited,adj)) return true;
-            }
+            if(visited[i] == 0)
+            if(bfs(i,visited,adj)) return true;
         }
         return false;
     }
     
-    public boolean CheckCycle(int node, int[] visited, ArrayList<ArrayList<Integer>> adj){
-        Queue<Pair> q = new LinkedList<>();
+    public boolean bfs(int node, int[] visited, ArrayList<ArrayList<Integer>> adj){
         visited[node] = 1;
-        q.add(new Pair(node,-1));
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(node,-1));
         
         while(!q.isEmpty()){
             Pair p = q.poll();
-            int nodes = p.src;
-            int parent = p.dest;
+            int src =  p.node;
+            int parents = p.parent;
             
-            for(int neighbor : adj.get(nodes)){
+            for(int neighbor : adj.get(src)){
                 if(visited[neighbor] == 0){
                     visited[neighbor] = 1;
-                    q.offer(new Pair(neighbor,nodes));
-                }else if(neighbor != parent){
-                    return true;
+                    q.offer(new Pair(neighbor,src));
+                }else{
+                    if(parents != neighbor) return true;
                 }
             }
         }
