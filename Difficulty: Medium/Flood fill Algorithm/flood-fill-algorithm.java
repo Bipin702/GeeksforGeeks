fdacf76503ef9ab3e86fbf0cdc6b25952e31b393
Bipin <bipinchoubey771@gmail.com>
2026-02-15 02:36:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.java
+++ Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.java
@@ -1,29 +1,34 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         // code here
-        int currColor = image[sr][sc];
-        int n = image.length;
+        int  n = image.length;
         int m = image[0].length;
-        int[][] result = image;
-
-        dfs(sr,sc,image,currColor,newColor,result);
-        return result;
+        int color = image[sr][sc];
+        
+        int[][] visited = new int[n][m];
+        
+        dfs(sr,sc,visited,image,color,newColor);
+        
+        return image;
     }
-
-    public void dfs(int row, int col, int[][] image, int currColor, int newColor, int[][] result){
-        result[row][col] = newColor;
+    
+    public void dfs(int sr, int sc, int[][] visited, int[][] image,int color, int newColor){
+        image[sr][sc] = newColor;
+        visited[sr][sc] = 1;
+        
         int n = image.length;
         int m = image[0].length;
-
+        
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
-
-        for(int d = 0; d < 4; d++){
-            int nRow = row + delRow[d];
-            int nCol = col + delCol[d];
-
-            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol] == currColor && result[nRow][nCol] != newColor){
-                dfs(nRow,nCol,image,currColor,newColor,result);
+        
+        for(int i = 0; i < 4; i++){
+            int nRow = sr + delRow[i];
+            int nCol = sc + delCol[i];
+            
+            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && visited[nRow][nCol] == 0 && image[nRow][nCol] == color){
+                image[nRow][nCol] = newColor;
+                dfs(nRow,nCol,visited,image,color,newColor);
             }
         }
     }
