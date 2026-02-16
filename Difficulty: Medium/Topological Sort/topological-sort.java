7e465e58de0bae2517e07b52af8cbb49bdfeee04
Bipin <bipinchoubey771@gmail.com>
2026-02-16 17:26:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -4,39 +4,48 @@ class Solution {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         
         for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        
-        for(int[] row : edges){
-            int u = row[0];
-            int v = row[1];
-            
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
             adj.get(u).add(v);
         }
         
-        Stack<Integer> st = new Stack<>();
-        
-        int[] visited = new int[V];
+        int[] indegree = new int[V];
         
         for(int i = 0; i < V; i++){
-            if(visited[i] == 0){
-                dfs(i,visited,adj,st);
-            }
+            for(int node : adj.get(i)) indegree[node]++;
         }
         
-        ArrayList<Integer> result = new ArrayList<>();
-        while(!st.isEmpty()){
-            result.add(st.pop());
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0) q.add(i);
         }
-        return result;
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans.add(node);
+            
+            for(int neighbor : adj.get(node)){
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    q.add(neighbor);
+                }
+            }
+        }
+        return ans;
     }
     
-    public void dfs(int node, int[] visited,ArrayList<ArrayList<Integer>> adj, Stack<Integer> st){
+    public void dfs(int node, int[] visited, Stack<Integer> st, ArrayList<ArrayList<Integer>> adj){
         visited[node] = 1;
         
         for(int neighbor : adj.get(node)){
-            if(visited[neighbor] == 0){
-                dfs(neighbor,visited,adj,st);
-            }
+            if(visited[neighbor] == 0)
+            dfs(neighbor,visited,st,adj);
         }
+        
         st.push(node);
     }
 }
