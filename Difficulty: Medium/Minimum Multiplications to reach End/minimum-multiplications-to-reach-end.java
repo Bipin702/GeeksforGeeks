ca53ff70c8667b6bc389ceee3ce994adbdf18780
Bipin <bipinchoubey771@gmail.com>
2026-02-22 00:52:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
@@ -3,42 +3,37 @@
 class Solution {
     class Pair{
         int node;
-        int step;
+        int count;
         
-        Pair(int node, int step){
+        Pair(int node, int count){
             this.node = node;
-            this.step = step;
+            this.count = count;
         }
     }
-    final int mod = 100000;
+    
     int minimumMultiplications(int[] arr, int start, int end) {
 
         // Your code here
+        if(start == end) return 0;
         int n = arr.length;
-        if (start == end) return 0;
-        if (arr == null || n == 0) return -1;
-        
-        int[] dist = new int[mod];
-        
-        Arrays.fill(dist,(int)1e9);
-        
-        dist[start] = 0;
-        
+        int mod = 100000;
+        boolean[] visited = new boolean[mod];
+        visited[start] = true;
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(start,0));
+        q.add(new Pair(start,0));
         
         while(!q.isEmpty()){
             Pair p = q.poll();
-            int nodes = p.node;
-            int steps = p.step;
+            int result = p.node;
+            int times = p.count;
             
-            for(int num : arr){
-                int next = (num*nodes)%mod;
-                
-                if(next == end) return steps+1;
-                if(steps+1 < dist[next]){
-                    dist[next] = steps+1;
-                    q.offer(new Pair(next,steps+1));
+            
+            for(int i = 0; i < n; i++){
+                int mul = (result * arr[i])%mod;
+                if(!visited[mul]){
+                    if(mul == end) return times+1;
+                    visited[mul] = true;
+                    q.add(new Pair(mul,times+1));
                 }
             }
         }
