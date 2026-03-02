4beafdaa63525f120ac4216f43e3ef2034e7dbc2
Bipin <bipinchoubey771@gmail.com>
2026-03-03 01:58:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number Of Islands/number-of-islands.java
+++ Difficulty: Medium/Number Of Islands/number-of-islands.java
@@ -1,86 +1,55 @@
 // User function Template for Java
-class DisjointSet{
-    ArrayList<Integer> parent = new ArrayList<>();
-    ArrayList<Integer> size = new ArrayList<>();
-
-    DisjointSet(int n){
-        for(int i = 0; i <= n; i++){
-            parent.add(i);
-            size.add(1);
-        }
-    }
-
-    public int findParent(int node){
-        if(node == parent.get(node)) return node;
-
-        int ulp = findParent(parent.get(node));
-        parent.set(node,ulp);
-
-        return parent.get(node);
-    }
-
-    public void unionBySize(int u, int v){
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-
-        if(ulp_u == ulp_v) return;
-
-        if(size.get(ulp_u) < size.get(ulp_v)){
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v, size.get(ulp_u)+ size.get(ulp_v));
-        } else{
-            parent.set(ulp_v,ulp_u);
-            size.set(ulp_u, size.get(ulp_u)+ size.get(ulp_v));
-        }
-    }
-}
-
+ 
 class Solution {
 
     public List<Integer> numOfIslands(int rows, int cols, int[][] operators) {
         // Your code here
-        DisjointSet ds = new DisjointSet(rows*cols);
-        
         List<Integer> ans = new ArrayList<>();
         
-        int[][] vis = new int[rows][cols];
+        int[][] grid = new int[rows][cols];
         
-        int n = operators.length;
+        for(int[] op : operators){
+            int r = op[0];
+            int c = op[1];
+            
+            grid[r][c] = 1;
+            
+            ans.add(countIslands(grid,rows,cols));
+        }
+        return ans;
+    }
+    
+    public int countIslands(int[][] grid, int rows, int cols){
+        int[][] visited = new int[rows][cols];
         
         int count = 0;
         
-        for(int i = 0; i < n ; i++){
-            int row  = operators[i][0];
-            int col = operators[i][1];
-            
-            if(vis[row][col] == 1){ 
-                ans.add(count);
-                continue;
-            }
-            vis[row][col] = 1;
-            count++;
-            
-            int[] delRow = {-1,0,1,0};
-            int[] delCol = {0,1,0,-1};
-            
-            for(int d = 0; d <4; d++){
-                int adjRow = row + delRow[d];
-                int adjCol =  col + delCol[d];
-                
-                if(adjRow < rows && adjRow >= 0 && adjCol < cols && adjCol >= 0){
-                    if(vis[adjRow][adjCol] == 1){ 
-                        int nodeNo = row * cols + col;
-                        int adjNo = adjRow * cols + adjCol;
-                        
-                        if(ds.findParent(nodeNo) != ds.findParent(adjNo)){
-                            count--;
-                            ds.unionBySize(nodeNo,adjNo);
-                        }
-                    }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 1 && visited[i][j] == 0){
+                    dfs(grid,i,j,visited); 
+                    count++;
                 }
             }
-            ans.add(count);
         }
-        return ans;
+        return count;
+    }
+    
+    public void dfs(int[][] grid, int row, int col, int[][] visited){
+        visited[row][col] = 1;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[] delRow = {-1,0,1,0};
+        int[] delCol = {0,1,0,-1};
+        
+        for(int i = 0; i < 4; i++){
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            
+            if(nRow >= 0 && nRow < rows && nCol >= 0 && 
+            nCol < cols && grid[nRow][nCol] == 1 && visited[nRow][nCol] == 0){
+                dfs(grid, nRow, nCol, visited);
+            } 
+        }
     }
 }
